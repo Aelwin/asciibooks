2434248097f537f5a13489859cc6d31e6536f0b5
unknown <juanlopez633@gmail.com>
2017-01-26 13:33:30 +0100

    creados datos iniciales

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,10 @@
+import com.asciibooks.*
+
 class BootStrap {
 
     def init = { servletContext ->
+    	def eric = new Author(name: "Eric", biography: "Grails Developer & Writer").save()
+		def grails3Book = new Book(author: eric, title: "Practical Grails 3", price: 100).save()
     }
     def destroy = {
     }
